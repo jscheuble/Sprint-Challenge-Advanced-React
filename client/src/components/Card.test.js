46b0bd23ef93be37382c318b24f226dbb7506014
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-03-13 10:22:58 -0700

    implement testing to card component

---
+++ client/src/components/Card.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Card from './Card';
+
+test('renders player data', async () => {
+    const { getByText } = render(<Card />);
+
+    const name = getByText(/name/i);
+    const country = getByText(/country/i);
+    const searches = getByText(/searches/i);
+
+    await expect(name).toBeDefined();
+    await expect(country).toBeDefined();
+    await expect(searches).toBeDefined();
+})
